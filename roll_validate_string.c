e985bbb1ecca8844777ffaa23eb4697aa823b879
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-04-08 20:01:35 +0200

    more adding to argument validation roll

--- roll_validate_string.c
+++ roll_validate_string.c
@@ -24,6 +24,29 @@ static int	ft_check_close_braces(char *string, int i,
 	return (0);
 }
 
+static int	ft_roll_check_arg(string)
+{
+	int	i;
+
+	i = 0;
+	while (string[i])
+	{
+		if (string[i] == '+' || string[i] == '-')
+			i++;
+		else if (string[i] == '/' || string[i] == '*')
+			i++;
+		else if (string[i] >= '0' || string[i] <= '9')
+			i++;
+		else if (string[i] == '(' || string[i] ==  ')')
+			i++;
+		else if (string[i] == 'd')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
 int	ft_command_roll_validate(char *string)
 {
 	int	open_braces;
@@ -33,14 +56,20 @@ int	ft_command_roll_validate(char *string)
 	i = 0;
 	open_braces = 0;
 	close_braces = 0;
+	if (ft_roll_check_arg(string))
+		return (1);
 	while (string[i])
 	{
+		while (string[i] >= '0' || string[i] <= '9')
+			i++;
 		if (string[i] == '(')
 			if (ft_check_open_braces(string, i, &open_braces))
 				return(1);
 		else if (string[i] == ')')
 			if (ft_check_close_braces(string, i, open_braces, &close_braces))
 				return (1);
+		else if (string[i] == '-' || string[i] == '+')
+		else if (string[i] == '/' || string[i] == '*')
 		i++;
 	}
 	return (0);
