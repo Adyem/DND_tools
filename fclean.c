7698718c7823bec0147f794477d8f7999984a596
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-04-13 15:48:31 +0200

    full fclean

--- fclean.c
+++ fclean.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fclean.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adyem <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 15:16:10 by adyem             #+#    #+#             */
+/*   Updated: 2024/04/13 15:37:32 by adyem            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "dnd_tools.h"
 
 void	ft_fclean(void)
 {
 	int		status; 
-	char	*command[5];
+	char	*command[4];
 	pid_t	pid;
 
 	command[0] = "/bin/sh";
 	command[1] = "-c";
-	command[2] = "rm -rf";
-	command[3] = "./data/*";
-	command[4] = NULL;
+	command[2] = "rm -rf ./data/*";
+	command[3] = NULL;
 	if (ft_read_line_yesorno("type yes for confirm or no to abort: "))
 			return ;
 	pid = fork();
