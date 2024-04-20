cecca98588ed5044501e80fc1e42b6202e93400e
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-04-20 19:16:47 +0200

    fixing out of bound reads in parse brackets

--- roll_parse_brackets.c
+++ roll_parse_brackets.c
@@ -7,17 +7,17 @@ int	ft_roll_parse_brackets(char *string, int *i, int j)
 	while (*i < j)
 	{
 		x = *i;
-		if (string[*i] == '(')
-			i++;
-		while (string[*i] >= '0' && string[*i] <= '9')
-			i++;
-		if (string[*i] == ')')
+		if (string && string[*i] == '(')
+			(*i)++;
+		while (string && (string[*i] >= '0' && string[*i] <= '9'))
+			(*i)++;
+		if (string && string[*i] == ')')
 		{
-			while (string[*i] != ')')
+			while (string && string[*i] != ')')
 				string[x] = string[x + 1];
-			if (string[x + 1] == ')')
+			if (string && string[x + 1] == ')')
 			{
-				while (string[x + 2])
+				while (string && string[x + 2])
 				{
 					string[x] = string[x + 2];
 					x++;
