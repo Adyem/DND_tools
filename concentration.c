2447840419ded2fefd4191777aaf39f894dcf698
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-06-08 20:56:09 +0200

    working on concentration

--- concentration.c
+++ concentration.c
@@ -12,43 +12,55 @@ static void	ft_concentration_remove_buf(t_char *info, t_char *target)
 	return ;
 }
 
-int	ft_remove_concentration(t_char *info)
+int ft_remove_concentration(t_char *info)
 {
-	int		i;
-	t_char	*target;
-	int		fd;
+    int i;
+    t_char *target;
+    int fd;
 
-	i = 0;
-	while (info->concentration.targets && info->concentration.targets[i])
+    i = 0;
+    while (info->concentration.targets && info->concentration.targets[i])
 	{
 		if (ft_set_stats_check_name(info->concentration.targets[i]))
 		{
-			i++;
-			continue ;
+			if (ft_check_player_character(info->concentration.targets[i]) == 0)
+			{
+				target = NULL;
+				i++;
+				continue;
+			} 
+			else
+				target = NULL;
 		}
-		target = ft_get_info(info->concentration.targets[i], info->struct_name);
-		if (!target)
-			return (ft_printf_fd(2, "300-Error getting info %s\n",
-					info->concentration.targets[i]));
-		ft_concentration_remove_buf(info, target);
-		fd = open(target->save_file, O_WRONLY | O_CREAT | O_TRUNC,
-				S_IRUSR | S_IWUSR);
-		if (fd == -1)
-			return (ft_printf_fd(2, "301-Error opening %s: %s\n",
-					info->save_file, strerror(errno)));
-		ft_npc_write_file(target, &target->stats, &info->c_resistance, fd);
-		ft_free_info(target);
-		close(fd);
-		break ;
-	}
-	info->concentration.concentration = 0;
-	info->concentration.spell_id = 0;
-	info->concentration.dice_amount_mod = 0;
-	info->concentration.dice_faces_mod = 0;
-	info->concentration.base_mod = 0;
-	ft_free_double_char(info->concentration.targets);
-	info->concentration.targets = NULL;
-	return (0);
+		else
+		{
+			target = ft_get_info(info->concentration.targets[i], info->struct_name);
+			if (!target)
+                return ft_printf_fd(2, "300-Error getting info %s\n", info->concentration.targets[i]);
+        }
+        if (target)
+		{
+            ft_concentration_remove_buf(info, target);
+            fd = open(target->save_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+            if (fd == -1)
+			{
+                ft_free_info(target);
+                return ft_printf_fd(2, "301-Error opening %s: %s\n", info->save_file, strerror(errno));
+            }
+            ft_npc_write_file(target, &target->stats, &info->c_resistance, fd);
+            ft_free_info(target);
+            close(fd);
+        }
+        i++;
+    }
+    info->concentration.concentration = 0;
+    info->concentration.spell_id = 0;
+    info->concentration.dice_amount_mod = 0;
+    info->concentration.dice_faces_mod = 0;
+    info->concentration.base_mod = 0;
+    ft_free_double_char(info->concentration.targets);
+    info->concentration.targets = NULL;
+    return 0;
 }
 
 void	ft_check_concentration(t_char *info, int damage)
