3d3893bb5db2201c3279a1ff0747aa377f144ca5
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-07-05 22:47:42 +0200

    adding chaos crystal turn

--- chaos_crystal.c
+++ chaos_crystal.c
@@ -1,9 +1,40 @@
 #include "dnd_tools.h"
+#include "libft/libft/libft.h"
+#include <fcntl.h>
+
+static void	ft_chaos_crystal_damage(t_char *info)
+{
+	char	**content;
+	int		fd;
+	int		length;
+	int		result;
+
+	fd = open("data/data--initiative", O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf_fd(2, "280-Error opening file: %s\n", strerror(errno));
+		return ;
+	}
+	content = ft_read_file_dnd(fd);
+	close(fd);
+	if (!content)
+	{
+		ft_printf_fd(2, "281-Error opening file: %s\n", strerror(errno));
+		return ;
+	}
+	length = ft_double_char_length(content);
+	result = -1;
+	while (result == -1 || ft_strncmp(content[result], "pc--", 4))
+		result = ft_dice_roll(1, length) - 1;
+	ft_printf("%s shoots a magic missle at %s and he/she takes 2 force damage\n",
+			info->name, &content[result][4]);
+	return ;
+}
 
 void	ft_chaos_crystal_turn(t_char *info)
 {
 	ft_update_buf(info);
-	ft_printf("The chaos_crystal will try to make either a ranged or melee attack during his turn\n");
+	ft_chaos_crystal_damage(info);
 	ft_printf("Chaos_crystal currently has %i/%i hp\n",
 			info->stats.health, info->dstats.health);
 }
