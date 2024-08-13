fc6644015e6f86595db63ed133dc9a10673a387e
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-08-13 21:22:33 +0200

    small changes to roll excecute

--- roll.c
+++ roll.c
@@ -1,5 +1,7 @@
 #include "dnd_tools.h"
 
+typedef int (*RollExecuteFunc)(char *, int *, int);
+
 void	ft_calculate_j(char *string, int *j)
 {
 	*j = 0;
@@ -8,14 +10,34 @@ void	ft_calculate_j(char *string, int *j)
 	return ;
 }
 
-static int	ft_command_roll_parse(char *string, int nested)
+int execute_roll_function(char *string, RollExecuteFunc func, const char *debug_tag, int nested)
+{
+	int i;
+	int	j;
+	int error;
+
+	i = 0;
+	error = 0;
+	ft_calculate_j(string, &j);
+	while (i < j)
+	{
+		error = func(string, &i, j);
+		if (DEBUG == 1)
+			printf("%s nested=%i i=%i j=%i\n", debug_tag, nested, i, j);
+		if (error)
+			return error;
+		i++;
+	}
+	return (0);
+}
+
+int ft_command_roll_parse(char *string, int nested)
 {
 	int error;
-	int	i;
-	int	j;
+	int i;
 
 	if (DEBUG == 1)
-		ft_printf("running ft_command_roll_parse\n");
+		printf("running ft_command_roll_parse\n");
 	i = 0 + nested;
 	while (string[i] != '(' && string[i])
 		i++;
@@ -23,57 +45,31 @@ static int	ft_command_roll_parse(char *string, int nested)
 	{
 		error = ft_command_roll_parse(&string[i], 1);
 		if (DEBUG == 1)
-			ft_printf("The value of error is %i\n", error);
+			printf("The value of error is %i\n", error);
 		if (error)
-			return (1);
+			return 1;
 	}
-	ft_calculate_j(string, &j);
-	i = 0;
-	while (i < j)
-	{
-		error = ft_roll_excecute_droll(string, &i, j);
-		if (DEBUG == 1)
-			ft_printf("DICE nested=%i i=%i j=%i\n", nested, i,  j);
-		if (error)
-			return (3);
-		i++;
-	}
-	ft_calculate_j(string, &j);
-	i = 0;
-	while (i < j)
-	{
-		error = ft_roll_excecute_md(string, &i, j);
-		if (DEBUG == 1)
-			ft_printf("MD nested=%i i=%i j=%i\n", nested, i,  j);
-		if (error)
-			return (4);
-		i++;
-	}
-	ft_calculate_j(string, &j);
-	i = 0;
-	while (i < j)
-	{
-		error = ft_roll_excecute_pm(string, &i, j);
-		if (DEBUG == 1)
-			ft_printf("PM nested=%i i=%i j=%i\n", nested, i,  j);
-		if (error)
-			return (5);
-		i++;
-	}
-	i = 0;
+	error = execute_roll_function(string, ft_roll_excecute_droll, "DICE", nested);
+	if (error)
+		return (3);
+	error = execute_roll_function(string, ft_roll_excecute_md, "MD", nested);
+	if (error)
+		return (4);
+	error = execute_roll_function(string, ft_roll_excecute_pm, "PM", nested);
+	if (error)
+		return (5);
 	if (DEBUG == 1)
-		ft_printf("nested is %i\n", nested);
+		printf("nested is %i\n", nested);
 	if (nested)
 	{
 		if (DEBUG == 1)
-			ft_printf("the string before parsing brackets is %s\n", string);
+			printf("the string before parsing brackets is %s\n", string);
 		error = ft_roll_parse_brackets(string);
 		if (error)
-			return (6);
-		i++;
+			return 6;
 	}
 	if (DEBUG == 1)
-		ft_printf("%s\n", string);
+		printf("%s\n", string);
 	return (0);
 }
 
