5697fa4c0882fd15fbacf2242be0485cf5e67d23
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-09-10 08:38:15 +0200

    changing printf to the c++ version

--- create_data_folder.cpp
+++ create_data_folder.cpp
@@ -3,32 +3,28 @@
 #include <cstring>
 #include <cerrno>
 #include <unistd.h>
-#include "libft/printf_fd/ft_printf_fd.hpp"
-#include "libft/printf/ft_printf.hpp"
+#include <iostream>
 
 int ft_create_data_dir()
 {
     struct stat st;
     
-	memset(&st, 0, sizeof(st));
+    memset(&st, 0, sizeof(st));
     if (stat("data", &st) == -1)
     {
-        if (errno == ENOENT)
-        {
-            if (mkdir("data", 0700) == -1)
-            {
-                ft_printf_fd(2, "001-Error failed to create directory: %s\n", strerror(errno));
+		if (errno == ENOENT)
+		{
+			if (mkdir("data", 0700) == -1)
+			{
+                std::cerr << "001-Error failed to create directory: " << strerror(errno) << std::endl;
                 return (1);
-            }
-            else
-            {
-                if (DEBUG == 1)
-                    ft_printf("Data folder created successfully\n");
-            }
-        }
-        else
+			}
+			else if (DEBUG == 1)
+				std::cout << "Data folder created successfully" << std::endl;
+		}
+		else
         {
-            ft_printf_fd(2, "002-Error failed to stat directory: %s\n", strerror(errno));
+            std::cerr << "002-Error failed to stat directory: " << strerror(errno) << std::endl;
             return (1);
         }
     }
@@ -36,17 +32,17 @@ int ft_create_data_dir()
     {
         if (access("data", R_OK | W_OK) == -1)
         {
-            ft_printf_fd(2, "003-Error no read/write access to 'data' directory: %s\n", strerror(errno));
+            std::cerr << "003-Error no read/write access to 'data' directory: " << strerror(errno) 
+                      << std::endl;
             return (1);
         }
         if (DEBUG == 1)
-            ft_printf("Data folder already exists with proper access rights\n");
+            std::cout << "Data folder already exists with proper access rights" << std::endl;
     }
     else
     {
-        ft_printf_fd(2, "004-Error path exists but is not a directory\n");
+        std::cerr << "004-Error path exists but is not a directory" << std::endl;
         return (1);
     }
     return (0);
 }
-
--- free_info.cpp
+++ free_info.cpp
@@ -1,11 +1,11 @@
 #include "dnd_tools.hpp"
 #include <cstdlib>
-#include "libft/printf/ft_printf.hpp"
+#include <iostream>
 
 void	ft_free_info(t_char *info)
 {
 	if (info && DEBUG == 1)
-		ft_printf("freeing info %s\n", info->name);
+		std::cout << "freeing info " << info->name << std::endl;
 	if (info && info->version_number >= 2)
 	{
 		ft_free_double_char(info->debufs.hunters_mark.caster_name);
