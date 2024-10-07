99aea8c63fa68540cc0d6d8261f198cb214002ac
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-10-07 08:13:08 +0200

    changing readline with rl_readline

--- initiative_request.cpp
+++ initiative_request.cpp
@@ -1,8 +1,9 @@
 #include "dnd_tools.hpp"
+#include "libft/CMA/CMA.hpp"
+#include "libft/ReadLine/readline.hpp"
 #include "libft/Libft/libft.hpp"
 #include "libft/Printf/ft_printf.hpp"
 #include <cstdlib>
-#include <readline/readline.h>
 #include <cstring>
 #include <cerrno>
 
@@ -13,31 +14,30 @@ int ft_request_initiative(t_pc *player)
     char *input;
     int initiative;
 
-    temp = ft_strjoin("Requesting initiative for ", player->name);
+	message = NULL;
+    temp = cma_strjoin("Requesting initiative for ", player->name, false);
     if (!temp)
     {
         ft_printf_fd(2, "250 Error allocating memory: %s\n", strerror(errno));
         return (1);
     }
-    message = ft_strjoin(temp, ": ");
-    free(temp);
+    message = cma_strjoin(temp, ": ", false);
+    cma_free(temp);
     if (!message)
     {
         ft_printf_fd(2, "251 Error allocating memory: %s\n", strerror(errno));
         return (1);
     }
-    while ((input = readline(message)))
+    while ((input = rl_readline(message)))
     {
-        if (!input)
-            return (1);
         if (ft_check_value(input))
         {
-            free(input);
+            cma_free(input);
             ft_printf("Requesting a number between 1 and 50\n");
             continue ;
         }
         initiative = ft_atoi(input);
-        free(input);
+        cma_free(input);
         if (!(initiative >= 1 && initiative <= 50))
         {
             ft_printf("Requesting a number between 1 and 50\n");
@@ -46,6 +46,6 @@ int ft_request_initiative(t_pc *player)
         player->initiative = initiative;
         break ;
     }
-    free(message);
+    cma_free(message);
     return (0);
 }
