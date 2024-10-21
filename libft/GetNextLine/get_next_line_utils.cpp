6f8e1b169add03a41b9c6eabbc763bcf96520700
Bastian Vangenechten <bvangene@a1-r3-p3.s19.be>
2024-10-21 11:39:30 +0200

    adding

--- libft/GetNextLine/get_next_line_utils.cpp
+++ libft/GetNextLine/get_next_line_utils.cpp
@@ -18,26 +18,28 @@ static char	*allocate_new_string(char *string_1, char *string_2, bool critical)
 	return (new_str);
 }
 
-char	*ft_strjoin_gnl(char *string_1, char *string_2, bool critical)
+char *ft_strjoin_gnl(char *string_1, char *string_2, bool critical)
 {
-	char	*result;
-	char	*original_string;
-	int		i;
+    char *result;
+    char *original_string;
+    int i;
+    char *temp1 = string_1;
+    char *temp2 = string_2;
 
-	if (!string_1 && !string_2)
-		return (nullptr);
-	original_string = string_1;
-	result = allocate_new_string(string_1, string_2, critical);
-	if (!result)
-		return (nullptr);
-	i = 0;
-	if (string_1)
-		while (*string_1)
-			result[i++] = *string_1++;
-	if (string_2)
-		while (*string_2)
-			result[i++] = *string_2++;
-	result[i] = '\0';
-	cma_free(original_string);
-	return (result);
+    if (!string_1 && !string_2)
+        return (nullptr);
+    original_string = string_1;
+    result = allocate_new_string(string_1, string_2, critical);
+    if (!result)
+        return (nullptr);
+    i = 0;
+    if (temp1)
+        while (*temp1)
+            result[i++] = *temp1++;
+    if (temp2)
+        while (*temp2)
+            result[i++] = *temp2++;
+    result[i] = '\0';
+    cma_free(original_string);
+    return (result);
 }
