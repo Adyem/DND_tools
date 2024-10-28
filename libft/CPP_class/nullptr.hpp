5fe2417a21f8caa51a09a59ce1a3e6ee558b6dbd
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-10-28 18:48:17 +0100

    small change to nullptr

--- libft/CPP_class/nullptr.hpp
+++ libft/CPP_class/nullptr.hpp
@@ -1,13 +1,18 @@
+#ifndef NULLPTR_HPP
+# define NULLPTR_HPP
+
 struct ft_nullptr_t
 {
-	    template <typename T>
-	    operator T*() const { return 0; }
+    template <typename T>
+    operator T*() const { return 0; }
 
-	    template <typename C, typename T>
-	    operator T C::*() const { return 0; }
+    template <typename C, typename T>
+    operator T C::*() const { return 0; }
 
-	private:
-	    void operator&() const;
+private:
+    void operator&() const;
 };
 
-const ft_nullptr_t ft_nullptr = {};
+#define ft_nullptr (ft_nullptr_t{})
+
+#endif
