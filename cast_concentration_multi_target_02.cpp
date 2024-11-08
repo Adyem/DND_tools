162147b7c93ef11ddc968d36dae7037584ca3353
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-11-08 10:24:55 +0100

    working on concentration

--- cast_concentration_multi_target_02.cpp
+++ cast_concentration_multi_target_02.cpp
@@ -1,7 +1,10 @@
+#include "character.hpp"
 #include "dnd_tools.hpp"
 
-void	ft_cast_concentration_multi_target_02(t_char *info, const char **input, t_buff *buff)
+void	ft_cast_concentration_multi_target_02(t_char *info, const char **input, t_buff *buff,
+			t_target_data target_data)
 {
+	(void)target_data;
 	(void)info;
 	(void)input;
 	(void)buff;
