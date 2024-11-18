e50bf03e196c7e58d71be196bd3e9e0a955e7b2f
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-11-18 19:41:23 +0100

    adding ft_perror

---
+++ libft/Errno/perror.cpp
@@ -0,0 +1,11 @@
+#include "errno.hpp"
+#include "../Printf/ft_printf.hpp"
+
+void	ft_perror(const char *error_msg)
+{
+	if (!error_msg)
+		pf_printf_fd(2, "%s", ft_strerror(ft_errno));
+	else
+		pf_printf_fd(2, "%s: %s", error_msg, ft_strerror(ft_errno));
+	return ;
+}
