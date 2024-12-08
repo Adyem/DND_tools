083e7c56d2971d4d8a9f93a4a6b5bdba55fc100c
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-12-08 11:45:36 +0100

    working on readline

--- libft/ReadLine/printeble_char.cpp
+++ libft/ReadLine/printeble_char.cpp
@@ -12,8 +12,8 @@ int rl_handle_printable_char(readline_state_t *state, int c, const char *prompt)
 	{
         int new_bufsize = state->bufsize * 2;
         state->buffer = rl_resize_buffer(state->buffer, state->bufsize, new_bufsize);
-		if (!state->buffer)
-			return (-1);
+        if (!state->buffer)
+            return (-1);
         state->bufsize = new_bufsize;
     }
     ft_memmove(&state->buffer[state->pos + 1], &state->buffer[state->pos],
@@ -21,12 +21,12 @@ int rl_handle_printable_char(readline_state_t *state, int c, const char *prompt)
     state->buffer[state->pos] = c;
     state->pos++;
     state->prev_buffer_length = ft_strlen(state->buffer);
-    pf_printf("\r%s", prompt);
-    pf_printf("%s", state->buffer);
+    rl_clear_line(prompt, state->buffer);
+    pf_printf("%s%s", prompt, state->buffer);
+
     int len_after_cursor = state->prev_buffer_length - state->pos;
-    if (len_after_cursor > 0) {
+    if (len_after_cursor > 0)
         pf_printf("\033[%dD", len_after_cursor);
-    }
     fflush(stdout);
-	return (0);
+    return (0);
 }
