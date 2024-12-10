63760690addca3939ce4b2bb8c7ebc6b6825b921
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-12-10 15:32:41 +0100

    working on spells

--- cast_divine_smite.cpp
+++ cast_divine_smite.cpp
@@ -18,19 +18,17 @@ void ft_cast_divine_smite(t_char *character)
     if (!ft_check_divine_smite_learned(character))
         return ;
     int base_level = character->spells.divine_smite.base_level;
-    int level = ft_prompt_spell_level(character, base_level);
-    if (level == -1)
+    int cast_at_level = ft_prompt_spell_level(character, base_level);
+    if (cast_at_level == -1)
         return ;
     t_divine_smite *divine_smite = &character->spells.divine_smite;
+	int upcast_level = base_level - cast_at_level;
     int total_dice = divine_smite->dice_amount
-                     + divine_smite->upcast_extra_dice_amount * level
-                     - divine_smite->upcast_extra_dice_amount;
-    int dice_faces = divine_smite->upcast_extra_dice_face
-                     * (level - divine_smite->base_level)
-                     - divine_smite->upcast_extra_dice_amount;
-    int extra_damage = divine_smite->upcast_extra_damage
-                       * (level - divine_smite->base_level)
-                       - divine_smite->upcast_extra_damage;
+                	+ (divine_smite->upcast_extra_dice_amount * upcast_level);
+    int dice_faces = divine_smite->dice_faces
+                    + (divine_smite->upcast_extra_dice_face * upcast_level);
+    int extra_damage = divine_smite->extra_damage
+                    + (divine_smite->upcast_extra_damage * upcast_level);
     int damage = ft_calculate_spell_damage(total_dice, dice_faces, extra_damage);
     if (damage == -1)
     {
@@ -38,7 +36,7 @@ void ft_cast_divine_smite(t_char *character)
         return ;
     }
     pf_printf("%s cast Divine Smite at level %i and dealt %i %s damage\n",
-            character->name, level, damage, DAMAGE_TYPE_RADIANT);
-	ft_remove_spell_slot(&character->spell_slots, level);
+            character->name, cast_at_level, damage, DAMAGE_TYPE_RADIANT);
+	ft_remove_spell_slot(&character->spell_slots, cast_at_level);
     return ;
 }
