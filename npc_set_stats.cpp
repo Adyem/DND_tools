a524688b68fcc98237f6710427bd9c31f763f465
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-12-22 21:41:22 +0100

    adding

--- npc_set_stats.cpp
+++ npc_set_stats.cpp
@@ -11,7 +11,8 @@ typedef struct s_key_value_triplet
     int unset_value;
 } t_key_value_triplet;
 
-static int ft_set_stat_int_internal(char *content, const char *key, int *field, int unset_value, ft_sharedptr<t_char> &info)
+static int ft_set_stat_int_internal(char *content, const char *key, int *field, int unset_value,
+		ft_sharedptr<t_char> &info)
 {
     int index = ft_strlen(key);
     if (ft_strncmp(content, key, index) == 0 && (unset_value == -1 || *field == unset_value))
@@ -22,15 +23,17 @@ static int ft_set_stat_int_internal(char *content, const char *key, int *field, 
     return (0);
 }
 
-static int ft_handle_int_mapping(char *line, const t_key_value_triplet *array, ft_sharedptr<t_char> &info)
+static int ft_handle_int_mapping(char *line, const t_key_value_triplet *stat_key_value_pairs,
+		ft_sharedptr<t_char> &info)
 {
-    int j = 0;
-    while (array[j].key)
+    int index = 0;
+    while (stat_key_value_pairs[index].key)
     {
-        if (ft_set_stat_int_internal(line, array[j].key, array[j].value, array[j].unset_value,
-					info) == 1)
+        if (ft_set_stat_int_internal(line, stat_key_value_pairs[index].key,
+				stat_key_value_pairs[index].value, stat_key_value_pairs[index].unset_value,
+				info) == 1)
             return (0);
-        j++;
+        index++;
     }
     return (1);
 }
@@ -76,7 +79,7 @@ static int ft_handle_string_fields(char *line, ft_sharedptr<t_char> &info)
 
 int ft_set_stats(ft_sharedptr<t_char> &info, char **content)
 {
-    t_key_value_triplet arr[] =
+    t_key_value_triplet stat_key_value_pairs[] =
     {
         {HEALTH_KEY, &info->stats.health, -1},
         {TEMP_HP_KEY, &info->stats.temp_hp, -1},
@@ -187,22 +190,22 @@ int ft_set_stats(ft_sharedptr<t_char> &info, char **content)
         {REACTION_USED_KEY, &info->flags.reaction_used, 0},
         {NULL, NULL, 0}
     };
-    int i = 0;
-    while (content[i])
+    int index = 0;
+    while (content[index])
     {
-        if (ft_handle_int_mapping(content[i], arr, info) == 0)
+        if (ft_handle_int_mapping(content[index], stat_key_value_pairs, info) == 0)
         {
-            i++;
+            index++;
             continue ;
         }
-        int err = ft_handle_string_fields(content[i], info);
-        if (err == 0)
+        int error_value = ft_handle_string_fields(content[index], info);
+        if (error_value == 0)
         {
-            i++;
+            index++;
             continue ;
         }
         pf_printf_fd(2, "1-Something is wrong with the save file for %s at the line: %s" \
-				", please reinitialize the save\n", info->name, content[i]);
+				", please reinitialize the save\n", info->name, content[index]);
         info->flags.error = 1;
         return (1);
     }
