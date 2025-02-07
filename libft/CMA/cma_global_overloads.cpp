0122237a323eaae4ec5fa15b4106c6f35992b446
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-02-07 10:16:00 +0100

    working on new and delete overloads

--- libft/CMA/cma_global_overloads.cpp
+++ libft/CMA/cma_global_overloads.cpp
@@ -4,7 +4,10 @@
 
 void* operator new(std::size_t size)
 {
-    return (cma_malloc(size));
+    void *pointer = cma_malloc(size);
+	if (!pointer)
+		throw std::bad_alloc();
+	return (pointer);
 }
 
 void* operator new(std::size_t size, const std::nothrow_t&) noexcept
@@ -32,7 +35,10 @@ void operator delete(void* ptr, const std::nothrow_t&) noexcept
 
 void* operator new[](std::size_t size)
 {
-    return (cma_malloc(size));
+    void *pointer = cma_malloc(size);
+	if (!pointer)
+		throw std::bad_alloc();
+	return (pointer);
 }
 
 void* operator new[](std::size_t size, const std::nothrow_t&) noexcept
@@ -53,6 +59,36 @@ void operator delete[](void* ptr, std::size_t) noexcept
 }
 
 void operator delete[](void* ptr, const std::nothrow_t&) noexcept
+{
+    cma_free(ptr);
+	return ;
+}
+
+void* operator new(std::size_t size, std::align_val_t align)
+{
+	(void)align;
+    void* pointer = cma_malloc(size);
+    if (!pointer)
+        throw std::bad_alloc();
+    return (pointer);
+}
+
+void operator delete(void* ptr, std::align_val_t) noexcept
+{
+    cma_free(ptr);
+	return ;
+}
+
+void* operator new[](std::size_t size, std::align_val_t align)
+{
+	(void)align;
+    void* pointer = cma_malloc(size);
+    if (!pointer)
+        throw std::bad_alloc();
+    return (pointer);
+}
+
+void operator delete[](void* ptr, std::align_val_t) noexcept
 {
     cma_free(ptr);
 	return ;
