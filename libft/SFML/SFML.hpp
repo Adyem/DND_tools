e1d1e443c26cf3cd00531c8d069fcb74f6e2a25d
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-02-17 22:46:37 +0100

    working on SFML wrapper library working on functions for the adding of sprites

--- libft/SFML/SFML.hpp
+++ libft/SFML/SFML.hpp
@@ -4,9 +4,11 @@
 #include <SFML/Graphics.hpp>
 #include "../Template/shared_ptr.hpp"
 #include "../Template/vector.hpp"
+#include <unordered_map>
+#include <string>
 
 #ifndef DEBUG
-# define DEBUG 0
+ #define DEBUG 0
 #endif
 
 typedef struct s_coordinate
@@ -17,38 +19,39 @@ typedef struct s_coordinate
 
 typedef struct s_object_coordinates
 {
-	t_coordinate virtual_location;
-	t_coordinate actual_location;
-	t_coordinate virtual_movement;
-	t_coordinate actual_movement;
+    t_coordinate virtual_location;
+    t_coordinate virtual_movement;
 } t_object_coordinates;
 
 typedef struct s_graphics_object
 {
-	ft_vector<ft_sharedptr<t_object_coordinates>> coordinates;
-	ft_sharedptr<sf::Drawable> drawable;
+    ft_vector<ft_sharedptr<t_object_coordinates>> coordinates;
+    ft_sharedptr<sf::Drawable> drawable;
 } t_graphics_object;
 
 class GraphicsData
 {
-	public:
-    	GraphicsData() = delete;
+    public:
+        GraphicsData() = delete;
+        GraphicsData(unsigned int width, unsigned int height, const char* title);
+        ~GraphicsData();
 
-    	GraphicsData(unsigned int width, unsigned int height, const char* title);
-    	~GraphicsData();
+        void addSpriteFromFile(const char *fileName);
+        void addSpriteFromFile(const char *fileName, const t_coordinate &position);
+        void addSprite(const sf::Texture &texture, const t_coordinate &position);
 
-		void addSprite(const sf::Texture &texture, const t_coordinate &position);
-		void addSpriteFromFile(const char *fileName);
-		void addSpriteFromFile(const char *fileName, const t_coordinate &position);
+        sf::RenderWindow *_window;
+        ft_vector<ft_sharedptr<t_graphics_object>> _object;
+        int _error;
 
-    	sf::RenderWindow *_window;
-		ft_vector<ft_sharedptr<t_graphics_object>> _object;
-		int _error;
+    private:
+        t_coordinate _window_size;
+        std::unordered_map<std::string, ft_sharedptr<sf::Texture>> _textureCache;
 
-	private:
-		t_coordinate _window_size;
+        t_coordinate translateCoordinates(const t_coordinate& customCoord) const;
 
-		t_coordinate translateCoordinates(const t_coordinate& customCoord) const;
+        sf::Texture* loadTexture(const char* fileName);
+        void addSpriteHelper(sf::Texture* texture, const t_coordinate* position);
 };
 
 #endif
--- libft/SFML/add_sprite.cpp
+++ libft/SFML/add_sprite.cpp
@@ -2,181 +2,121 @@
 #include <SFML/Graphics.hpp>
 #include <new>
 #include <string>
+#include <unordered_map>
 
-void GraphicsData::addSpriteFromFile(const char *fileName)
+sf::Texture* GraphicsData::loadTexture(const char* fileName)
 {
-    try
+    std::string fname(fileName);
+    auto it = _textureCache.find(fname);
+    if (it != _textureCache.end())
+        return it->second.get();
+    sf::Texture* newTexture = new(std::nothrow) sf::Texture();
+    if (!newTexture || !newTexture->loadFromFile(fileName))
     {
-        sf::Texture* texture = new(std::nothrow) sf::Texture();
-        if (!texture->loadFromFile(fileName))
-        {
-            delete texture;
-            this->_error = 1;
-            return;
-        }
-        for (size_t i = 0; i < _object.size(); ++i)
-        {
-            sf::Drawable* drawable = _object[i]->drawable.get();
-            sf::Sprite* sprite = dynamic_cast<sf::Sprite*>(drawable);
-            if (sprite)
-                if (sprite->getTexture() == texture)
-                    return;
-        }
-        sf::Sprite* newSprite = new sf::Sprite();
-        newSprite->setTexture(*texture);
-
-        ft_sharedptr<t_graphics_object> gfxObj(new t_graphics_object());
-        if (gfxObj.getErrorCode())
-        {
-            this->_error = 1;
-            return;
-        }
-        gfxObj->drawable = ft_sharedptr<sf::Drawable>(newSprite);
-        this->_object.push_back(gfxObj);
-        if (this->_object.getError())
-            this->_error = 1;
+        delete newTexture;
+        return nullptr;
     }
-    catch (...)
+    ft_sharedptr<sf::Texture> texturePtr(newTexture);
+    if (texturePtr.getErrorCode())
     {
-        this->_error = 1;
+        delete newTexture;
+        return nullptr;
     }
-    return;
+    _textureCache[fname] = texturePtr;
+    return newTexture;
 }
 
-void GraphicsData::addSpriteFromFile(const char *fileName, const t_coordinate &position)
+void GraphicsData::addSpriteHelper(sf::Texture* texture, const t_coordinate* position)
 {
-    try
+    bool hasPosition = (position != nullptr);
+    t_coordinate sfCoord;
+    if (hasPosition)
+        sfCoord = translateCoordinates(*position);
+    for (size_t i = 0; i < _object.size(); ++i)
     {
-        sf::Texture* texture = new(std::nothrow) sf::Texture();
-        if (!texture->loadFromFile(fileName))
+        sf::Drawable* drawable = _object[i]->drawable.get();
+        sf::Sprite* sprite = dynamic_cast<sf::Sprite*>(drawable);
+        if (sprite && sprite->getTexture() == texture)
         {
-            delete texture;
-            this->_error = 1;
-            return ;
-        }
-        t_coordinate sfCoord = translateCoordinates(position);
-        for (size_t i = 0; i < _object.size(); ++i)
-        {
-            sf::Drawable* drawable = _object[i]->drawable.get();
-            sf::Sprite* sprite = dynamic_cast<sf::Sprite*>(drawable);
-            if (sprite)
-            {
-                if (sprite->getTexture() == texture)
-                {
-                    ft_sharedptr<t_object_coordinates> coords(new t_object_coordinates());
-                    if (coords.getErrorCode())
-                    {
-                        this->_error = 1;
-                        return ;
-                    }
-                    coords->virtual_location = sfCoord;
-                    coords->actual_location  = sfCoord;
-                    _object[i]->coordinates.push_back(coords);
-                    if (_object[i]->coordinates.getError())
-                    {
-                        this->_error = 1;
-                        return ;
-                    }
-                    return ;
-                }
-            }
-        }
-        sf::Sprite* newSprite = new sf::Sprite();
-        newSprite->setTexture(*texture);
-        newSprite->setPosition(sfCoord.x, sfCoord.y);
-        ft_sharedptr<t_graphics_object> gfxObj(new t_graphics_object());
-        if (gfxObj.getErrorCode())
-        {
-            this->_error = 1;
-            return ;
-        }
-        gfxObj->drawable = ft_sharedptr<sf::Drawable>(newSprite);
-        if (gfxObj->drawable.getErrorCode())
-        {
-            this->_error = 1;
-            return ;
-        }
-        ft_sharedptr<t_object_coordinates> coords(new t_object_coordinates());
-        if (coords.getErrorCode())
-        {
-            this->_error = 1;
-            return ;
-        }
-        coords->virtual_location = sfCoord;
-        coords->actual_location  = sfCoord;
-        gfxObj->coordinates.push_back(coords);
-        if (gfxObj.getErrorCode())
-        {
-            this->_error = 1;
-            return ;
-        }
-        this->_object.push_back(gfxObj);
-        if (this->_object.getError())
-            this->_error = 1;
-    }
-    catch (...)
-    {
-        this->_error = 1;
-    }
-    return ;
-}
-
-
-void GraphicsData::addSprite(const sf::Texture &texture, const t_coordinate &position)
-{
-    try
-    {
-        for (size_t i = 0; i < _object.size(); ++i)
-        {
-            sf::Drawable* drawable = _object[i]->drawable.get();
-            sf::Sprite* sprite = dynamic_cast<sf::Sprite*>(drawable);
-            if (sprite && sprite->getTexture() == &texture)
+            if (hasPosition)
             {
                 ft_sharedptr<t_object_coordinates> coords(new t_object_coordinates());
                 if (coords.getErrorCode())
                 {
                     this->_error = 1;
-                    return ;
+                    return;
                 }
+                coords->virtual_location = sfCoord;
                 _object[i]->coordinates.push_back(coords);
                 if (_object[i]->coordinates.getError())
                 {
                     this->_error = 1;
-                    return ;
+                    return;
                 }
-                return ;
             }
+            return;
         }
-        sf::Sprite* newSprite = new sf::Sprite();
-        newSprite->setTexture(texture);
-        t_coordinate sfCoord = translateCoordinates(position);
+    }
+    sf::Sprite* newSprite = new sf::Sprite();
+    newSprite->setTexture(*texture);
+    if (hasPosition)
         newSprite->setPosition(sfCoord.x, sfCoord.y);
-        ft_sharedptr<t_graphics_object> gfxObj(new t_graphics_object());
-        gfxObj->drawable = ft_sharedptr<sf::Drawable>(newSprite);
-        if (gfxObj->drawable.getErrorCode())
-        {
-            this->_error = 1;
-            return ;
-        }
+    ft_sharedptr<t_graphics_object> gfxObj(new t_graphics_object());
+    if (gfxObj.getErrorCode())
+    {
+        this->_error = 1;
+        return;
+    }
+    gfxObj->drawable = ft_sharedptr<sf::Drawable>(newSprite);
+    if (gfxObj->drawable.getErrorCode())
+    {
+        this->_error = 1;
+        return;
+    }
+    if (hasPosition)
+    {
         ft_sharedptr<t_object_coordinates> coords(new t_object_coordinates());
         if (coords.getErrorCode())
         {
             this->_error = 1;
-            return ;
+            return;
         }
+        coords->virtual_location = sfCoord;
         gfxObj->coordinates.push_back(coords);
-        if (gfxObj.getErrorCode())
+        if (gfxObj->coordinates.getError())
         {
             this->_error = 1;
-            return ;
+            return;
         }
-        this->_object.push_back(gfxObj);
-        if (this->_object.getError())
-            this->_error = 1;
     }
-    catch (...)
+    _object.push_back(gfxObj);
+    if (_object.getError())
+        this->_error = 1;
+}
+
+void GraphicsData::addSpriteFromFile(const char *fileName)
+{
+    sf::Texture* texture = loadTexture(fileName);
+    if (!texture)
     {
         this->_error = 1;
+        return;
     }
-    return ;
+    addSpriteHelper(texture, nullptr);
+}
+
+void GraphicsData::addSpriteFromFile(const char *fileName, const t_coordinate &position)
+{
+    sf::Texture* texture = loadTexture(fileName);
+    if (!texture)
+    {
+        this->_error = 1;
+        return;
+    }
+    addSpriteHelper(texture, &position);
+}
+
+void GraphicsData::addSprite(const sf::Texture &texture, const t_coordinate &position)
+{
+    addSpriteHelper(const_cast<sf::Texture*>(&texture), &position);
 }
