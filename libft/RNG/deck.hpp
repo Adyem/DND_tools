56e96eee475848d799e9733c33e97be29a4248db
Bastian Vangenechten <bvangene@a1-r3-p3.s19.be>
2025-03-04 15:30:06 +0100

    removing constructors and destructors from deck it uses the ones from ft_vector

--- libft/RNG/deck.hpp
+++ libft/RNG/deck.hpp
@@ -10,39 +10,12 @@ template<typename ElementType>
 class deck : public ft_vector<ElementType*>
 {
 	public:
-    	deck();
-		~deck();
-    
-    	ElementType *getRandomElement();
     	ElementType *popRandomElement();
+		ElementType *getRandomElement() const;
 		void shuffle();
 		ElementType *drawTopElement();
+		ElementType *peekTopElement() const;
 };
-
-template<typename ElementType>
-deck<ElementType>::deck() : ft_vector<ElementType*>()
-{
-	return ;
-}
-
-template<typename ElementType>
-deck<ElementType>::~deck()
-{
-	return ;
-}
-
-template<typename ElementType>
-ElementType *deck<ElementType>::getRandomElement()
-{
-    if (this->empty())
-	{
-		ft_errno = DECK_EMPTY;
-        this->setError(DECK_EMPTY);
-		return (ft_nullptr);
-	}
-    size_t index = static_cast<size_t>(ft_dice_roll(1, static_cast<int>(this->size())) - 1);
-    return ((*this)[index]);
-}
 
 template<typename ElementType>
 ElementType *deck<ElementType>::popRandomElement()
@@ -60,6 +33,19 @@ ElementType *deck<ElementType>::popRandomElement()
 }
 
 template<typename ElementType>
+ElementType *deck<ElementType>::getRandomElement() const
+{
+    if (this->empty())
+	{
+		ft_errno = DECK_EMPTY;
+        this->setError(DECK_EMPTY);
+		return (ft_nullptr);
+	}
+    size_t index = static_cast<size_t>(ft_dice_roll(1, static_cast<int>(this->size())) - 1);
+    return ((*this)[index]);
+}
+
+template<typename ElementType>
 ElementType *deck<ElementType>::drawTopElement()
 {
     if (this->empty())
@@ -71,6 +57,20 @@ ElementType *deck<ElementType>::drawTopElement()
 	size_t index = this->size() - 1;
     ElementType* elem = (*this)[index];
 	this->release_at(index);
+    return (elem);
+}
+
+template<typename ElementType>
+ElementType *deck<ElementType>::peekTopElement() const
+{
+    if (this->empty())
+	{
+		ft_errno = DECK_EMPTY;
+		this->setError(DECK_EMPTY);
+		return (ft_nullptr);
+	}
+	size_t index = this->size() - 1;
+    ElementType* elem = (*this)[index];
     return (elem);
 }
 
