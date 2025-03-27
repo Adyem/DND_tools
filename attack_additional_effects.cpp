bba40c4a8c2118872daecdc49f2fd1ea695d6df0
Bastian Vangenechten <bvangene@a1-r3-p3.s19.be>
2025-03-27 14:00:07 +0100

    snall change to additional effects starting to add multi attack for the ancient beast for the war of the ancients campaign

--- attack_additional_effects.cpp
+++ attack_additional_effects.cpp
@@ -1,11 +1,15 @@
 #include "character.hpp"
 #include "dnd_tools.hpp"
+#include "identification.hpp"
 
 void	ft_attack_additional_effects(t_char *info, t_equipment_id *weapon, bool is_hit, int is_crit,
 								int offhand)
 {
 	(void)offhand;
+	(void)is_crit;
 	if (is_hit && weapon->action_01.function)
-		weapon->action_01.function(info, weapon, is_crit);
+		weapon->action_01.function(info, weapon, 0);
+	if (weapon->action_01.effect_type == ETYPE_ANCIENT_PREDATORY_BEAST_MULTI_ATTACK)
+		weapon->action_01.function(info, weapon, 0);
 	return ;
 }
