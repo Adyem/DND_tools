d19997c254ad109181e000ab8b88d4f1bbc684c4
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-07-23 16:29:02 +0200

    trying to fix fel poison

--- fel_poison_weapon_effects.cpp
+++ fel_poison_weapon_effects.cpp
@@ -1,51 +1,17 @@
 #include "character.hpp"
 #include "dnd_tools.hpp"
-#include "libft/CMA/CMA.hpp"
 #include "libft/Printf/printf.hpp"
 #include "libft/RNG/dice_roll.hpp"
-#include "libft/CPP_class/nullptr.hpp"
-
-static char *ft_fel_poison_attack_effect_construct_message(t_char *info)
-{
-    char *string_constitution = cma_itoa(ft_calculate_dex(info));
-    if (!string_constitution)
-    {
-        pf_printf_fd(2, "148-Error: Allocation failure %s poison effect\n", info->name);
-        return (ft_nullptr);
-    }
-    char *prefix = cma_strdup("the target needs to succeed on a DC15");
-    if (!prefix)
-    {
-        pf_printf_fd(2, "149-Error: Allocation failure %s poison effect\n", info->name);
-        cma_free(string_constitution);
-        return (ft_nullptr);
-    }
-    char *temp = cma_strjoin(prefix, string_constitution);
-    cma_free(prefix);
-    if (!temp)
-    {
-        pf_printf_fd(2, "150-Error: Allocation failure %s poison effect\n", info->name);
-        cma_free(string_constitution);
-        return (ft_nullptr);
-    }
-    char *message = cma_strjoin(temp, " constitution save or become poisoned");
-    cma_free(temp);
-    cma_free(string_constitution);
-    if (!message)
-    {
-        pf_printf_fd(2, "151-Error: Allocation failure %s poison effect\n", info->name);
-        return (ft_nullptr);
-    }
-    return (message);
-}
 
 void ft_fel_poison_attack_effects(t_char *info, t_equipment_id *weapon, t_equipment_effect *effect,
 		t_attack_info *attack_info)
 {
+	(void)info;
 	(void)effect;
 	if (!attack_info->is_hit)
 		return ;
-    char *message = ft_fel_poison_attack_effect_construct_message(info);
+    const char *message = "the target needs to succeed on a DC15 constitution " \
+					 "save or become poisoned";
     if (!message)
         return ;
     if (ft_readline_confirm(message))
@@ -55,6 +21,5 @@ void ft_fel_poison_attack_effects(t_char *info, t_equipment_id *weapon, t_equipm
         pf_printf("The target is poisoned by fell poison, taking %i damage and "
                   "suffering disadvantage on their attacks for the next 2 turns.", result);
     }
-    cma_free(message);
     return ;
 }
