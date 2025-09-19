658f2f1caef9866ccbbc4a77e706ac1708e26428
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-19 19:42:49 +0200

    Use ft_bzero to clear character names

--- initialize.cpp
+++ initialize.cpp
@@ -3,13 +3,7 @@
 
 void ft_initialize_variables(t_target_data *target_data)
 {
-    int index = 0;
-
     target_data->buff_info = ft_nullptr;
-    while (index < 20)
-    {
-        target_data->Pchar_name[index] = ft_nullptr;
-        index++;
-    }
+    ft_bzero(target_data->Pchar_name, sizeof(target_data->Pchar_name));
     return ;
 }
